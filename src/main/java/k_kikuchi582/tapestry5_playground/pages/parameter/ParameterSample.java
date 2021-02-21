bcefdc4b267dd529ed424d0b8ddd58e4c0764a19
k-kikuchi582 <k.kikuchi.balsam@gmail.com>
2021-02-21 23:39:16 +0900

    Parameterについてのページを修正

--- src/main/java/k_kikuchi582/tapestry5_playground/pages/parameter/ParameterSample.java
+++ src/main/java/k_kikuchi582/tapestry5_playground/pages/parameter/ParameterSample.java
@@ -1,15 +1,35 @@
 package k_kikuchi582.tapestry5_playground.pages.parameter;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.tapestry5.Asset;
+import org.apache.tapestry5.annotations.Path;
 import org.apache.tapestry5.annotations.Persist;
 import org.apache.tapestry5.annotations.Property;
+import org.apache.tapestry5.ioc.annotations.Inject;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 public class ParameterSample {
-
     @Persist
     @Property
     private int count;
 
+    @Inject
+    @Path("./parameters")
+    private Asset parametersClassContent;
+
     private String getNull() {
         return null;
+    }
+
+    public String getParametersClassContent() throws IOException {
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try (InputStream input = parametersClassContent.getResource().openStream()) {
+            IOUtils.copy(input, output);
+        }
+        return output.toString();
     }
 }
