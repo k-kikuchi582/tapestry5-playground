19083f461ce3c68fe63ebd01c7037afb4cc590e2
k-kikuchi582 <k.kikuchi.balsam@gmail.com>
2021-10-26 00:19:38 +0900

    Sampleコンポーネントの修正に伴って、不要になったパラメータを削除

--- src/main/java/k_kikuchi582/tapestry5_playground/pages/component/catalog/form/Radio.java
+++ src/main/java/k_kikuchi582/tapestry5_playground/pages/component/catalog/form/Radio.java
@@ -1,33 +1,4 @@
 package k_kikuchi582.tapestry5_playground.pages.component.catalog.form;
 
-import k_kikuchi582.tapestry5_playground.internal.AdditionalSampleItem;
-import k_kikuchi582.tapestry5_playground.util.Utils;
-import org.apache.tapestry5.Asset;
-import org.apache.tapestry5.Block;
-import org.apache.tapestry5.annotations.Id;
-import org.apache.tapestry5.annotations.Path;
-import org.apache.tapestry5.ioc.annotations.Inject;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.List;
-
 public class Radio {
-    @Inject
-    @Id("radioSamplePropertiesBlock")
-    private Block radioSamplePropertiesBlock;
-
-    @Inject
-    @Path("../../../../components/component/catalog/form/RadioSample.properties")
-    private Asset radioSampleProperties;
-
-    public List<AdditionalSampleItem> getAdditionalItems() {
-        return Collections.singletonList(
-                new AdditionalSampleItem("RadioSample.properties", radioSamplePropertiesBlock)
-        );
-    }
-
-    public String getRadioSamplePropertiesText() throws IOException {
-        return Utils.getText(radioSampleProperties);
-    }
 }
